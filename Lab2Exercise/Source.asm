d2b64db4dcdcc9ed0be3ded9f2e1b9cca48320ec
dit20152@office365.uop.gr <dit20152@office365.uop.gr>
2022-08-20 15:50:12 +0300

    Declare variables

--- Lab2Exercise/Source.asm
+++ Lab2Exercise/Source.asm
@@ -3,6 +3,23 @@ INCLUDE Irvine32.inc
 
 .data
 
+mCalculating    BYTE "Calculating...",odh,oah,0
+mAccessElement  BYTE "The value offset the element a[2,2] is : ",0
+mResultSum      BYTE "The sum a[2,2]+z= :",0
+mResultDiff     BYTE "The sum a[2,2]-z= :",0
+
+array1 SDWORD 00d ,10d ,20d, 30d
+ElementsPerRow = ($-array1)/TYPE array1
+       SDWORD 01d, 11d, 21d, 31d
+       SDWORD 02d, 12d, 22d, 32d
+       SDWORD 03d, 13d, 23d, 33d
+
+x BYTE 2d
+y BYTE 2d
+
+z SDWORD -30d
+
+
 
 .code 
 main PROC
